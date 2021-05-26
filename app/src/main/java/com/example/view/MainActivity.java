d914c875b0419a57d1016830f6a4df56257d4f41
LouiseFuglsang <louise.mortensen@gmail.com>
2021-05-26 21:33:32 +0200

    added bottom_navigation_menu

--- app/src/main/java/com/example/view/MainActivity.java
+++ app/src/main/java/com/example/view/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.new_yorkerdk.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +19,32 @@ public class MainActivity extends AppCompatActivity {
     public void changeActivityToProductCatalog(View view) {
         setContentView(R.layout.productcatalog);
     }
+
+    BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+
+                case R.id.page_1:
+                break;
+                    // do something here
+                    return true;
+
+                case R.id.page_2:
+                    // do something here
+                    break;
+                    return true;
+
+                case R.id.page_3:
+                    // do something here
+                    break;
+                    return true;
+                default: return true;
+            }
+        }
+    });
+    
+
 }
