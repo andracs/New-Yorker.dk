db533652d7c4ba6e12a0eddd86a5363d8c5ff032
sasaje <sara590x@edu.easj.dk>
2021-05-27 09:53:58 +0200

    Design changes in activity_main.xml, background image and top logo added.

--- app/src/main/java/com/example/view/MainActivity.java
+++ app/src/main/java/com/example/view/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.new_yorkerdk.R;
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove the title bar from activity_main
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); //hide actionbar
         setContentView(R.layout.activity_main);
     }
 
