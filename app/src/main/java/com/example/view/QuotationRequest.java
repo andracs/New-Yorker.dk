73a41151dd80754b273bd38941a8481e50d8e912
sasaje <sara590x@edu.easj.dk>
2021-05-27 21:42:49 +0200

    Layout for quotation_request.xml added.

--- app/src/main/java/com/example/view/QuotationRequest.java
+++ app/src/main/java/com/example/view/QuotationRequest.java
@@ -3,14 +3,46 @@ package com.example.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.new_yorkerdk.R;
 
-public class QuotationRequest extends AppCompatActivity {
+public class QuotationRequest extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quotation_request);
+
+        Spinner widthspinner = (Spinner) findViewById(R.id.width_spinner);
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.width_spinner, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+        widthspinner.setAdapter(adapter);
+        widthspinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void changeActivityToProductCatalog(View view) {
+        setContentView(R.layout.activity_main);
     }
 }
