823add8b05fcd0f4d0fd7d9da0983b9befcd0656
sasaje <sara590x@edu.easj.dk>
2021-05-27 14:25:01 +0200

    Changes in productcatalog.xml added and new activity activity_tilbudsforespoergsel.xml added.

---
+++ app/src/main/java/com/example/view/ProductCatalog.java
@@ -0,0 +1,16 @@
+package com.example.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.new_yorkerdk.R;
+
+public class ProductCatalog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.productcatalog);
+    }
+}
