ab4c9533b9dd4137869e10e564633550a1d293eb
LouiseFuglsang <louise.mortensen@gmail.com>
2021-05-28 11:50:46 +0200

    BottomNavigationMenu added

--- app/src/main/java/com/example/view/Contact.java
+++ app/src/main/java/com/example/view/Contact.java
@@ -2,33 +2,27 @@ package com.example.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.new_yorkerdk.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class Contact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_contact);
 
         //BottomNavigationMenu start
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(2);
         menuItem.setChecked(true);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,29 +32,22 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
 
                     case R.id.page_1:
+                        Intent intent0 = new Intent(Contact.this, MainActivity.class);
+                        startActivity(intent0);
                         break;
 
                     case R.id.page_2:
-                        Intent intent1 = new Intent(MainActivity.this, ProductCatalog.class);
+                        Intent intent1 = new Intent(Contact.this, ProductCatalog.class);
                         startActivity(intent1);
                         break;
 
+
                     case R.id.page_3:
-                        Intent intent2 = new Intent(MainActivity.this, Contact.class);
-                        startActivity(intent2);
                         break;
                 }
                 return false;
             }
         });
-    }
-    //BottomNavigationMenu end
-
-    public void changeActivityToProductCatalog(View view) {
-        setContentView(R.layout.product_catalog);
-    }
-
-    public void changeActivityToQuotationRequest(View view) {
-        setContentView(R.layout.quotation_request);
+        //BottomNavigationMenu end
     }
 }
--- app/src/main/java/com/example/view/ProductCatalog.java
+++ app/src/main/java/com/example/view/ProductCatalog.java
@@ -1,12 +1,17 @@
 package com.example.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.new_yorkerdk.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProductCatalog extends AppCompatActivity {
 
@@ -15,9 +20,42 @@ public class ProductCatalog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_catalog);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Groups history");
         Log.d("TAGS", "Title = " + toolbar.getTitle());
-    }
+
+
+        //BottomNavigationMenu start
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.page_1:
+                        Intent intent0 = new Intent(ProductCatalog.this, MainActivity.class);
+                        startActivity(intent0);
+                        break;
+
+                    case R.id.page_2:
+                        break;
+
+                    case R.id.page_3:
+                        Intent intent2 = new Intent(ProductCatalog.this, Contact.class);
+                        startActivity(intent2);
+
+                        break;
+                }
+                return false;
+            }
+        });
+        //BottomNavigationMenu end
+        }
+
 }
