d8631f81a2a430f9bd741104df386254121d933e
András Ács <5522871+andracs@users.noreply.github.com>
2021-06-03 12:17:22 +0200

    Update GlassWall.java
    Forslag til fremgangsmåde

--- app/src/main/java/model/GlassWall.java
+++ app/src/main/java/model/GlassWall.java
@@ -11,5 +11,16 @@ public class GlassWall {
     String colour;
     int totalPrice;
 
+    // TODO Lav constructor
+    // TODO Lav getters setters
+    // NEXT STEP Onclick --> Konstruer objektet -> Log.d() objektet (TAG: "Forstå")
+    // I opdager den mangler en toString(); --> "Elementets navn er Single Fag, den har bredde 0, højde 0, farve default."
+    // Sende det her objekt til nlæste aktivity
+    // Indlæse objektet og set GUI'et sådan så det passer 
+    // Bruger indtaster værdier i GUI, --> så set objektets egenskaber 
+    // Log.d() objektet (TAG: "Forstå")
+    // Sende det her objekt til nlæste aktivity (LÆG DET i KURVEN?)
 
+    
+    
 }
